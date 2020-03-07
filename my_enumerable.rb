1bf4f5c50643556285fec9f1c158af9a4aec8569
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-07 21:00:17 +0100

    add my each

---
+++ my_enumerable.rb
@@ -0,0 +1,12 @@
+# rubocop:disable Style/CaseEquality, Metrics/ModuleLength, Style/For
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for element in self
+      yield(element)
+    end
+  end
+end
+
+# rubocop:enable Style/CaseEquality, Metrics/ModuleLength, Style/For
