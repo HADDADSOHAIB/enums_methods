bc74dddbf06731a25691cf06f6d198aa1e4e674a
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-10 19:07:25 +0100

    test my_select

--- spec/my_enumerable_specs.rb
+++ spec/my_enumerable_specs.rb
@@ -2,7 +2,7 @@ require './my_enumerable.rb'
 
 RSpec.describe Enumerable do
   let(:expected_value){ { 'ok' => 'ok', 0 => 0, 1 => 1, 2 => 2, 3 => 3, 1.5 => 1.5, false => false, nil => nil, :a=>:a } }
-
+  
   describe '#my_each' do
     ['ok', 1, 1.5, false, nil, :a].my_each do |item|
       it "return the same item: #{item} from inside the array" do
@@ -32,7 +32,7 @@ RSpec.describe Enumerable do
       end
     end
 
-    {'ok' => 1, 'ok' => 1, 1.5 => 1, false => :a }.my_each do |k, v, i|
+    {'ok' => 1, :a => 0, 1.5 => 1, false => :a }.my_each do |k, v, i|
       it "return the same key: #{k} and value: #{v} and the index from inside the hash" do
         expect(v).to eq(expected_value[v])
         expect(k).to eq(expected_value[k])
@@ -45,6 +45,20 @@ RSpec.describe Enumerable do
         expect(item).to eq(expected_value[item])
         expect(i).to eq(expected_value[i])
       end
+    end
+  end
+
+  describe "#my_select" do
+    it "return only the true values for an array" do
+      expect([true, 1, "ok", false, nil].my_select{ |item| item}).to eq([true, 1, "ok"])
+    end
+
+    it "return only the true values for a hash" do
+      expect({'ok' => 1, :a => 1, 1.5 => nil, false => :a}.my_select{ |k, v| k && v}).to eq({'ok' => 1, :a => 1})
+    end
+
+    it "return only the true values for a range" do
+      expect((1..10).my_select{ |value| value < 5}).to eq([1, 2, 3, 4])
     end
   end
 end
