c111620e3dae008cdb79782758f8e80c4b881315
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-10 18:13:32 +0100

    test my_each_with_index

--- spec/my_enumerable_specs.rb
+++ spec/my_enumerable_specs.rb
@@ -1,9 +1,9 @@
 require './my_enumerable.rb'
 
 RSpec.describe Enumerable do
-  let(:expected_value){ { 'ok' => 'ok', 1 => 1, 2 => 2, 3 => 3, 1.5 => 1.5, false => false, nil => nil, :a=>:a } }
+  let(:expected_value){ { 'ok' => 'ok', 0 => 0, 1 => 1, 2 => 2, 3 => 3, 1.5 => 1.5, false => false, nil => nil, :a=>:a } }
 
-  describe '#my-each' do
+  describe '#my_each' do
     ['ok', 1, 1.5, false, nil, :a].my_each do |item|
       it "return the same item: #{item} from inside the array" do
         expect(item).to eq(expected_value[item])
@@ -20,6 +20,30 @@ RSpec.describe Enumerable do
     (1..3).my_each do |item|
       it "return the same item: #{item} from inside the range" do
         expect(item).to eq(expected_value[item])
+      end
+    end
+  end
+
+  describe '#my_each_with_index' do
+    ['ok', 1.5, :a, false].my_each do |item, i|
+      it "return the same item: #{item} and the index from inside the array" do
+        expect(item).to eq(expected_value[item])
+        expect(i).to eq(expected_value[i])
+      end
+    end
+
+    {'ok' => 1, 'ok' => 1, 1.5 => 1, false => :a }.my_each do |k, v, i|
+      it "return the same key: #{k} and value: #{v} and the index from inside the hash" do
+        expect(v).to eq(expected_value[v])
+        expect(k).to eq(expected_value[k])
+        expect(i).to eq(expected_value[i])
+      end
+    end
+
+    (1..3).my_each do |item, i|
+      it "return the same item: #{item} and the index from inside the range" do
+        expect(item).to eq(expected_value[item])
+        expect(i).to eq(expected_value[i])
       end
     end
   end
